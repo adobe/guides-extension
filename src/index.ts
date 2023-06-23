69c8f83d02908c06797afdb26ca404b7916ed33c
Ishank Sharma <ishank.web.dev@gmail.com>
2023-06-23 14:59:27 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,8 @@ NOTICE: Adobe permits you to use, modify, and distribute this file in
 accordance with the terms of the Adobe license agreement accompanying
 it.
 */
-import * as loader from "./loader";
 import repository_panel from "./repository_panel";
-import comment from "./comment";
-export { default as repository_panel } from "./repository_panel";
-export { default as Comment } from "./comment";
 
-tcx?.extension?.register("repository_panel", "repository_panel");
+export { default as repository_panel } from "./repository_panel";
+
 tcx?.extension?.register("repository_panel", repository_panel);
-tcx?.extension?.register("comment", comment);
